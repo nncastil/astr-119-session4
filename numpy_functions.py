4c3efea62bae8e1f3615e5a8ebc51b9b5c821ce1
nncastil <43651616+nncastil@users.noreply.github.com>
2018-10-09 11:25:12 -0700

    10/9

---
+++ numpy_functions.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+x = 1.0
+y = 2.0
+
+#Trigonometric
+print(np.sin(x))
+print(np.cos(x))
+print(mp.tan(x))
+print(np.arcsin(x))
+print(np.arccos(x))
+print(np.arctan(x))
+print(np.arctan2(x,y))
+print(np.rad2deg(x))
+
+#Hyperbolic
+print(np.sinh(x))
+print(np.cosh(x))
+print(np.tan(h))
